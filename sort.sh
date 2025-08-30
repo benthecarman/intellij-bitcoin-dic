f8dd9bcc7f24efe54730d300051214d1573b79c9
benthecarman <benthecarman@live.com>
2025-08-29 19:14:03 -0500

    Make sort work from other dirs

--- sort.sh
+++ sort.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Sort bitcoin.dic file alphabetically
-LC_ALL=C sort -u bitcoin.dic -o bitcoin.dic
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LC_ALL=C sort -u "$SCRIPT_DIR/bitcoin.dic" -o "$SCRIPT_DIR/bitcoin.dic" || exit 1
 
 echo "bitcoin.dic has been sorted alphabetically"
